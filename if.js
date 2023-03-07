9fc6f94c468e3939750924c7a7a6763abd38e0e5
albertodiaz <carlosalberto.diazjimenez@gmail.com>
2023-03-06 20:47:33 -0500

    load repository

---
+++ if.js
@@ -0,0 +1,10 @@
+// Sentencia IF 
+let operacion = 0 + 1;
+
+if (operacion > 7) {
+    console.log('Es mayor a 7');
+} else if (operacion < 7 && operacion > 2) {
+        console.log('Está entre 7 y 2')
+    } else {
+        console.log('Es menor de 2')
+    }
---
+++ switch.js
@@ -0,0 +1,14 @@
+// Sentencia switch
+
+let operacionS = 5 + 9 
+
+switch(operacionS) {
+    case 0:
+        console.log("Es Cero");
+    break;
+    case 7:
+        console.log("Es Siete");
+    break;
+    default: 
+    console.log("El resultado no es ni cero noi siete")
+}
